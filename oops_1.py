b96202c135d6c1c6af338105cadafdf2f6de4724
Virendra8181 <sisodiyavirendra321@gmial.com>
2023-02-09 23:37:52 +0530

    my_code

---
+++ oops_1.py
@@ -0,0 +1,22 @@
+
+
+class Sbi:
+    amount=1000
+    
+    def __init__(self):
+        print(self.amount)
+        
+    
+    
+    def deposit(self,val):
+        b=val
+        
+
+    def customer(self):
+        pass
+
+s=Sbi()
+
+
+s.deposit(1000)
+s.customer()
---
+++ oops_2.py
@@ -0,0 +1,15 @@
+
+from unicodedata import name
+
+
+class Sbi:
+    gidline="follow arbi gides line"
+    def __init__(self,name,id):
+        cus=name
+        ID=id
+    
+s=Sbi("mahendra singh",101)
+r=Sbi("gajendra singh",202)
+print(s.gidline)
+print(s.cus)
+print(s.Id)
